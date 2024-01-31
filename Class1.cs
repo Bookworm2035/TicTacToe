306b9d8ce6c831aebf3029ea46384fba432c1620
Amy Fox <amyfox723@gmail.com>
2024-01-31 13:40:36 -0700

    added methods

--- Class1.cs
+++ Class1.cs
@@ -7,15 +7,26 @@ using System.Threading.Tasks;
 namespace TicTacToe
 {
 
-    /*) The supporting class (with whichever name you choose) will:
-• Receive the “board” array from the driver class
-• Contain a method that prints the board based on the information passed to it
-• Contain a method that receives the game board array as input and returns if there is a
-winner and who it was
- */
+     /*) The supporting class (with whichever name you choose) will:
+        • Receive the “board” array from the driver class
+        • Contain a method that prints the board based on the information passed to it
+        • Contain a method that receives the game board array as input and returns if there is a
+        winner and who it was
+         */
     internal class Class1
     {
+        public Array PrintBoard(Array board)
+        {
 
+            return board;
+        }
+
+        public string WhoWon(Array board)
+        {
+            string result = "";
+
+            return result;
+        }
 
     }
 }
