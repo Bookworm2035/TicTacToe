d737710e1186775cfb946bad530a42024352e8c8
Bookworm2035 <126933809+Bookworm2035@users.noreply.github.com>
2024-01-31 14:10:09 -0700

    added array (part of it)

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Lili added this");
 
 
 
-    /* The “Driver” class (the Program.cs class with the main method where the program begins) 
+/* The “Driver” class (the Program.cs class with the main method where the program begins) 
 will:
 • Welcome the user to the game
 • Create a game board array to store the players’ choices
@@ -16,16 +16,35 @@ will:
 when a win has occurred and which player won the game */
 
 //welcome user
-Console.WriteLine("Welcome")
+Console.WriteLine("Welcome to the best game of TicTacToe of your life");
 
-//Board array 
 
-// Ak for choice and update array
+while GAME IS NOT OVER DO THIS
 
+bool isPlayerX = true; 
+
+//CreateBoard array and declare variables
+string [] boardArray = new string[9];
+string moveChoice ;
+
+// Ask for choice and update array
+Console.Write("Player X will start. Select your move, insert a number 1-9");
+moveChoice= Console.ReadLine();
+
+
+
+
+bool isPlayerX = false;
 //print board my calling Class1 method BOARD
 
 //check for winner, calling method in Class1
 
 // notify players when a win has occurred adn who won 
 
-    
+
+
+
+ //when game ends end while loop
+
+
+//display stuff
