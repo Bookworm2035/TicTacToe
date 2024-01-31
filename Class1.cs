182da02c7d0957bbc64fafe27f20ffba96e303cd
liliantsubaki <liliantsubaki@gmail.com>
2024-01-31 14:38:12 -0700

    We have completed da stuff we think

--- Class1.cs
+++ Class1.cs
@@ -31,27 +31,30 @@ namespace TicTacToe
         {
             string result = "";
 
-            if ((board[0] == 'X' && board[1] == 'X' && board[2] == 'X') || 
-                (board[3] == 'X' && board[4] == 'X' && board[5] == 'X') ||
-                (board[6] == 'X' && board[7] == 'X' && board[8] == 'X') ||
-                (board[0] == 'X' && board[3] == 'X' && board[6] == 'X') ||
-                (board[1] == 'X' && board[4] == 'X' && board[7] == 'X') ||
-                (board[2] == 'X' && board[5] == 'X' && board[8] == 'X') ||
-                (board[0] == 'X' && board[4] == 'X' && board[8] == 'X') ||
-                (board[2] == 'X' && board[4] == 'X' && board[6] == 'X'))
+            string[] winningCombinations = {
+                "012", "345", "678",  // Horizontal
+                "036", "147", "258",  // Vertical
+                "048", "246"          // Diagonal
+            };
+
+            foreach (string combination in winningCombinations)
             {
-                result = "Player X won!";
-            }
-            else if ((board[0] == 'O' && board[1] == 'O' && board[2] == 'O') ||
-                    (board[3] == 'O' && board[4] == 'O' && board[5] == 'O') ||
-                    (board[6] == 'O' && board[7] == 'O' && board[8] == 'O') ||
-                    (board[0] == 'O' && board[3] == 'O' && board[6] == 'O') ||
-                    (board[1] == 'O' && board[4] == 'O' && board[7] == 'O') ||
-                    (board[2] == 'O' && board[5] == 'O' && board[8] == 'O') ||
-                    (board[0] == 'O' && board[4] == 'O' && board[8] == 'O') ||
-                    (board[2] == 'O' && board[4] == 'O' && board[6] == 'O'))
-            {
-                result = "Player O won!";
+                char first = board[combination[0]];
+                char second = board[combination[1]];
+                char third = board[combination[2]];
+
+                if (first == 'X' && second == 'X' && third == 'X')
+                {
+                    result = "Player X won!";
+                }
+                else if (first == 'O' && second == 'O' && third == 'O')
+                {
+                    result = "Player O won!";
+                }
+                else
+                {
+                    result = "Next player's turn!";
+                }
             }
 
             return result;
