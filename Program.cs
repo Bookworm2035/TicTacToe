2829c915630188f3af2229e972b7fd437c4e826d
liliantsubaki <liliantsubaki@gmail.com>
2024-01-31 13:31:00 -0700

    Lili changed this

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("DID THIS FINALY WORK PLS SAY YES CRIESSSSS")
+Console.WriteLine("DID THIS FINALY WORK PLS SAY YES CRIESSSSS");
+Console.WriteLine("Lili did this");
