4ae0868ff2e65d56961e724137c73c0f4e60f4a9
Bookworm2035 <126933809+Bookworm2035@users.noreply.github.com>
2024-01-31 13:40:18 -0700

    commmittt

--- Program.cs
+++ Program.cs
@@ -13,4 +13,18 @@ will:
 • Print the board by calling the method in the supporting class
 • Check for a winner by calling the method in the supporting class, and notify the players 
 when a win has occurred and which player won the game */
+
+//welcome user
+Console.WriteLine("Welcome")
+
+//Board array 
+
+// Ak for choice and update array
+
+//print board my calling Class1 method BOARD
+
+//check for winner, calling method in Class1
+
+// notify players when a win has occurred adn who won 
+
     
