c81f237659ff9e1dcd866f83af1eb765adfd60e3
Bookworm2035 <126933809+Bookworm2035@users.noreply.github.com>
2024-02-01 15:28:22 -0700

    try to fix array

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using TicTacToe;
 
 /* The “Driver” class (the Program.cs class with the main method where the program begins) 
@@ -9,16 +10,61 @@ will:
 • Print the board by calling the method in the supporting class
 • Check for a winner by calling the method in the supporting class, and notify the players 
 when a win has occurred and which player won the game */
-char[] board = new char[9];
-char moveChoice;
+//change this to 10 so we can use 1-9 positions and 0 will not be used 
+char[] board = new char[10];
+// Assign values to the spots in the array
+for (int i = 0; i <= 9; i++)
+{
+    board[i] = Convert.ToChar(i); // You can replace 'X' with the desired initial value
+}
+Console.WriteLine(board[1]);
+Console.WriteLine(board[2]);
 
+int moveChoice2;
+int moveChoice;
+bool GameWon = false;
 // Create an instance of Class1
 Class1 c = new Class1();
 
 // Call PrintBoard method
 c.PrintBoard(board);
 
-// Ask for choice and update array
-Console.Write("Player X will start. Select your move, insert a number 1-9: ");
-moveChoice = Convert.ToChar(Console.ReadLine());
 
+while (GameWon == false) {
+    //PUT THE LOOP ALL
+
+    // Ask for choice and update array
+    bool PlayerX = true;
+
+    Console.Write("Player X will start. Select your move, insert a number 1-9: ");
+    moveChoice = Convert.ToInt32(Console.ReadLine());
+    //debug check variable value
+    Console.WriteLine(moveChoice);
+
+    //add X or O to array spot based on player name
+    //numbers[positionToAdd] = valueToAdd;
+    if (PlayerX == true) {
+       board[moveChoice] = 'X';
+    }
+    else {
+        board[moveChoice] = 'O';
+    }
+
+    //other players turn 
+    PlayerX = false;
+    Console.Write("Player O's turn. Select your move, insert a number 1-9: ");
+    moveChoice2 = Convert.ToInt32(Console.ReadLine());
+
+    if (PlayerX == true)
+    {
+        board[moveChoice2] = 'X';
+    }
+    else
+    {
+        board[moveChoice2] = 'O';
+    }
+    Console.WriteLine(board);
+}
+
+//after there is a winner
+//display stuff here :) 
